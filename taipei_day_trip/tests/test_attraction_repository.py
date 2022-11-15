c0fa21500cf6f84ae2eb40e05ddfb46777413833
vkmouse <knight31241@gmail.com>
2022-11-15 21:25:51 +0800

    refactor: refactor test case of attraction

--- taipei_day_trip/tests/test_attraction_repository.py
+++ taipei_day_trip/tests/test_attraction_repository.py
@@ -4,21 +4,23 @@ from taipei_day_trip.core import UnitOfWork
 from taipei_day_trip.repository import MemoryUnitOfWork
 from taipei_day_trip.repository import MySQLUnitOfWork
 
-def attraction_test_case(db: UnitOfWork):
-    db.categories.add('category1')
-    db.mrts.add('mrt1')
-    db.mrts.add('mrt2')
-    assert len(db.attractions.get_all()) == 0
-
-    assert db.attractions.add(name='attr1',
+def add_attraction(db: UnitOfWork, name: str = None, category: str = None, mrt: str = None) -> bool:
+    return db.attractions.add(name='attr1' if name == None else name,
                               description='desc1',
                               address='addr',
                               lat=50,
                               lng=40,
                               transport='trans',
-                              images=[],
-                              category='category1',
-                              mrt='mrt2') == True
+                              images=['123', '456'],
+                              category='category1' if category == None else category,
+                              mrt=mrt)
+
+def attraction_test_case(db: UnitOfWork):
+    db.categories.add('category1')
+    db.mrts.add('mrt1')
+    db.mrts.add('mrt2')
+    assert len(db.attractions.get_all()) == 0
+    assert add_attraction(db, mrt='mrt2') == True
     all = db.attractions.get_all()
     assert len(all) == 1
     assert all[0].id == 1
@@ -28,32 +30,15 @@ def attraction_test_case(db: UnitOfWork):
     assert all[0].lat == 50
     assert all[0].lng == 40
     assert all[0].transport == 'trans'
-    assert all[0].images == []
+    assert all[0].images == ['123', '456']
     assert all[0].category == 'category1'
     assert all[0].mrt == 'mrt2'
-
-    assert db.attractions.add(name='attr1',
-                              description='desc1',
-                              address='addr',
-                              lat=50,
-                              lng=40,
-                              transport='trans',
-                              images=[],
-                              category='category2',
-                              mrt='mrt2') == False
+    assert add_attraction(db, mrt='mrt2', category='category2') == False
 
 def images_test_case(db: UnitOfWork):
     db.categories.add('category1')
     db.mrts.add('mrt1')
-    assert db.attractions.add(name='attr1',
-                              description='desc1',
-                              address='addr',
-                              lat=50,
-                              lng=40,
-                              transport='trans',
-                              images=['123', '456'],
-                              category='category1',
-                              mrt='mrt1') == True
+    assert add_attraction(db) == True
     actual = db.attractions.get_all()[0].images
     assert len(actual) == 2
     assert actual[0] == '123'
@@ -61,37 +46,13 @@ def images_test_case(db: UnitOfWork):
 
 def null_mrt_test_case(db: UnitOfWork):
     db.categories.add('category1')
-    assert db.attractions.add(name='attr1',
-                              description='desc1',
-                              address='addr',
-                              lat=50,
-                              lng=40,
-                              transport='trans',
-                              images=['123', '456'],
-                              category='category1',
-                              mrt=None) == True
+    assert add_attraction(db, mrt=None) == True
     assert db.attractions.get_all()[0].mrt == None
 
 def get_by_id_test_case(db: UnitOfWork):
     db.categories.add('category1')
-    db.attractions.add(name='attr1',
-                       description='desc1',
-                       address='addr',
-                       lat=50,
-                       lng=40,
-                       transport='trans',
-                       images=['123', '456'],
-                       category='category1',
-                       mrt=None)
-    db.attractions.add(name='attr2',
-                       description='desc1',
-                       address='addr',
-                       lat=50,
-                       lng=40,
-                       transport='trans',
-                       images=['123', '456'],
-                       category='category1',
-                       mrt=None)
+    assert add_attraction(db, name='attr1') == True
+    assert add_attraction(db, name='attr2') == True
     assert db.attractions.get_by_id(1).name == 'attr1'
     assert db.attractions.get_by_id(2).name == 'attr2'
 
