a166d21f6d886747a8103c131ac236f170bb9b39
vkmouse <knight31241@gmail.com>
2022-11-17 21:03:42 +0800

    chore: update deployment files

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -f status=exited -q)
 sudo docker rmi $(sudo docker images -a -q)
+sudo docker volume rm $(sudo docker volume ls -q)
 sudo docker system prune -f
 sudo docker compose up -d
