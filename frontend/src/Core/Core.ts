5059866eadcff68178781574b8f82a918f37bb2c
vkmouse <knight31241@gmail.com>
2022-11-22 23:09:33 +0800

    feat(core): add core type add mockapi

---
+++ frontend/src/Core/Core.ts
@@ -0,0 +1,12 @@
+export interface Attraction {
+  id: number
+  name: string
+  description: string
+  address: string
+  lat: number
+  lng: number
+  transport: string
+  images: string[]
+  category: string
+  mrt?: string
+}
