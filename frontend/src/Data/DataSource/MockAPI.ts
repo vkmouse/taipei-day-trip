32ecd239b4e737994f4138ced6eb2a4ab8d22d94
vkmouse <knight31241@gmail.com>
2022-11-23 13:12:32 +0800

    feat(date): add get categories mock api

--- frontend/src/Data/DataSource/MockAPI.ts
+++ frontend/src/Data/DataSource/MockAPI.ts
@@ -1,33 +1,54 @@
 import { Attraction } from "../../Core/Core";
 
+const getMockAttraction = (id: number) : Attraction => {
+  return {
+    id: id,
+    name: `樹河${id}`,
+    description: '',
+    address: '',
+    lat: 0,
+    lng: 0,
+    transport: '',
+    images: ['https://www.travel.taipei/content/images/attractions/221371/1024x768_attractions-image-jyms0r6aquqilooahyiw8w.jpg'],
+    category: `公共藝術${id}`,
+    mrt: `忠孝復興${id}`
+  };
+};
+
 const getAttractions = async (page: number, keyword: string = '') : Promise<{ data: Attraction[], nextPage: number | null }> => {
     let data: Attraction[] = [];
     let nextPage: number | null = page + 1;
-
     for (let i = 0; i < 12; i++) {
       const id = i + 12 * page;
-      const attraction: Attraction = {
-        id,
-        name: `樹河${id}`,
-        description: '',
-        address: '',
-        lat: 0,
-        lng: 0,
-        transport: '',
-        images: ['https://www.travel.taipei/content/images/attractions/221371/1024x768_attractions-image-jyms0r6aquqilooahyiw8w.jpg'],
-        category: `公共藝術${id}`,
-        mrt: `忠孝復興${id}`
-      };
-      data.push(attraction);
+      data.push(getMockAttraction(i));
     }
 
-    if (page > 3 || (keyword !== '' && page > 1)) {
+    if (keyword === '' && page > 3) {
       nextPage = null;
       data = data.slice(1, 6);
+    } else if (keyword !== '' && page > 0) {
+      nextPage = null;
+      data = data.slice(1, 6);
+    } else if (keyword === '1') {
+      nextPage = null;
+      data = data.slice(1, 5);
     }
+
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve({ data, nextPage }), 1000);
   });
 };
 
-export { getAttractions };
+const getCategories = async () : Promise<{ data: string[] }> => {
+  const data = [
+    '綠野現蹤', '你是笨蛋', '哈哈哈哈', '輸入景點',
+    '綠野現蹤', '你是笨蛋', '哈哈哈哈', '輸入景點',
+    '綠野現蹤', '你是笨蛋', '哈哈哈哈', '輸入景點',
+    '綠野現蹤', '你是笨蛋', '哈哈哈哈', '輸入景點',
+  ];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({ data }), 1000);
+  });
+};
+
+export { getAttractions, getCategories };
