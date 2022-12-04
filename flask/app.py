0b1932dd862404a058e7d7a44eb51c55dd0b24c6
vkmouse <knight31241@gmail.com>
2022-12-04 23:37:47 +0800

    refactor: remove html

--- flask/app.py
+++ flask/app.py
@@ -17,19 +17,5 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.register_blueprint(attraction_bp(db))
 app.register_blueprint(category_bp(db))
 
-# Pages
-@app.route("/")
-def index():
-	return render_template("index.html")
-@app.route("/attraction/<id>")
-def attraction(id):
-	return render_template("attraction.html")
-@app.route("/booking")
-def booking():
-	return render_template("booking.html")
-@app.route("/thankyou")
-def thankyou():
-	return render_template("thankyou.html")
-
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3000)
