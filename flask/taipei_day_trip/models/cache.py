ce79d845346c8ac23789742211d7f4b5ac9a3778
vkmouse <knight31241@gmail.com>
2022-12-10 09:50:23 +0800

    feat: add expire time to cache

--- flask/taipei_day_trip/models/cache.py
+++ flask/taipei_day_trip/models/cache.py
@@ -1,5 +1,5 @@
 class Cache:
     def get(self, key: any):
         return NotImplemented
-    def set(self, key: any, value: any):
+    def set(self, key: any, value: any, exp: int = 0):
         return NotImplemented
--- flask/taipei_day_trip/models/memory/cache.py
+++ flask/taipei_day_trip/models/memory/cache.py
@@ -1,14 +1,25 @@
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
 from taipei_day_trip.models.cache import Cache
 
 class MemoryCache(Cache):
     def __init__(self):
         self.__db = {}
+        self.__exp = {}
 
     def get(self, key: any):
+        now = datetime.now(tz=timezone.utc)
         try:
-            return self.__db[key]
+            if self.__exp[key] > now:
+                return self.__db[key]
+            else:
+                return None
         except:
             return None
 
-    def set(self, key: any, value: any):
+    def set(self, key: any, value: any, exp: int = 0):
+        expires = datetime.now(tz=timezone.utc)
+        expires += timedelta(seconds=int(exp))
         self.__db[key] = value
+        self.__exp[key] = expires
